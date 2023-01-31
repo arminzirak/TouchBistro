2b3499c7d845080b52d65830d005111e94ceb3cf
Armin <armin.zirak97@gmail.com>
2023-01-31 16:56:59 -0700

    config files

--- configs.py
+++ configs.py
@@ -5,5 +5,13 @@ DATA_DIRECTORY = './data/'
 OUTPUT_DIRECTORY = './output/'
 
 COVID_CASES_ADDRESS = f'{DATA_DIRECTORY}/covid_cases.csv'
+RESTAURANTS_REVENUE_ADDRESS = f'{DATA_DIRECTORY}/takehome_exercise_v2.xlsx'
+RESTAURANTS_REVENUE_SHEET = 'Sample Restaurant Revenue'
+
+RESULTS_ADDRESS = f'{OUTPUT_DIRECTORY}/results.csv'
 
 COVID_API_URL = 'https://api.opencovid.ca/timeseries'
+
+SELECTED_PROVINCE = 'ON'
+
+FIRST_DAY_OF_WEEK = 4  # Friday
