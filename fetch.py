aadb6b5742aeaba157db23381398cb5e4f6fea45
Armin <armin.zirak97@gmail.com>
2023-01-31 16:55:52 -0700

    fetch error handling + refactoring

--- fetch.py
+++ fetch.py
@@ -1,13 +1,14 @@
 import requests
-from configs import WINDOW_START, WINDOW_END, COVID_CASES_ADDRESS, COVID_API_URL
+from configs import WINDOW_START, WINDOW_END, COVID_CASES_ADDRESS, COVID_API_URL, SELECTED_PROVINCE
 
 
 def fetch_covid_cases():
-    print(COVID_API_URL)
     result = requests.get(COVID_API_URL,
                           params={'fmt': 'csv', 'legacy': 'false', 'hr_names': 'hruid', 'pt_names': 'short',
                                   'fill': 'true', 'after': WINDOW_START, 'before': WINDOW_END,
-                                  'loc': 'ON', 'geo': 'pt', 'stat': 'cases'})
+                                  'loc': SELECTED_PROVINCE, 'geo': 'pt', 'stat': 'cases'})
+    if result.status_code != 200:
+        raise RuntimeError('API Call Status Code : {}'.format(result.status_code))
+
     f = open(COVID_CASES_ADDRESS, 'w')
     f.write(result.content.decode('UTF-8'))
-    return True
