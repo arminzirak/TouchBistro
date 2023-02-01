27ea7299e7ac13e48280c9c4254651a86a2df826
Armin <armin.zirak97@gmail.com>
2023-01-31 19:01:27 -0700

    SQL refactored

--- employee_rates.sql
+++ employee_rates.sql
@@ -1,8 +1,1 @@
---version 1
-select employee_id, sum((extract(EPOCH from "end_time"-"start_time") / 3600) * hourly_wage) from employee_activity group by employee_id
-
---version 2
-select employee_id, sum((extract(EPOCH from LEAST("end_time", '2022-04-17 17:00:00')-GREATEST("start_time", '2022-04-15 09:00:00')) / 3600) * hourly_wage) from employee_activity where ("end_time" is not null or "start_time" >= '2022-04-14 21:00:00') group by employee_id;
-
---version 3
 select employee_id, sum(GREATEST((extract(EPOCH from LEAST("end_time", '2022-04-17 17:00:00')-GREATEST("start_time", '2022-04-15 09:00:00')) / 3600), 0) * hourly_wage) from employee_activity where ("end_time" is not null or "start_time" >= '2022-04-14 21:00:00') group by employee_id;
